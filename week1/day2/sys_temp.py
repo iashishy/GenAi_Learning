264632a5eab1064911c4ac86f2764bdc34ec259b
Ashish Yadav <ashish150105@gmail.com>
2026-07-15 06:31:54 +0530

    Complete Week 1 Day 1 and Day 2

---
+++ week1/day2/sys_temp.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key = os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha hai bhai")
+
+client=Groq(api_key=my_api_key)
+model="llama-3.3-70b-versatile"
+role="user"
+prompt="Suggest a name for my clothing company "
+#system
+message_system={
+    "role": "system",
+    "content": "You are a brand manager who suggests name for my clothing comapany. name should be in one word. suggest only one name"
+}
+message={
+    "role": role,
+    "content": prompt
+}
+messages=[message_system,message]
+# temperature by default is 0 meaning safe
+
+response=client.chat.completions.create(model=model, messages=messages, temperature=2)
+#print(response)
+
+print("####################################")
+answer=response.choices[0].message.content
+print(answer)
